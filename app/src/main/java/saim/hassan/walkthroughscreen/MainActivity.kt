5f51868688b85b1fe6f2345337045e0f1f575d99
SAIM <saimh355@gmail.com>
2021-01-17 21:24:21 +0500

    Walkthrough Screen using ViewPager 2

--- app/src/main/java/saim/hassan/walkthroughscreen/MainActivity.kt
+++ app/src/main/java/saim/hassan/walkthroughscreen/MainActivity.kt
@@ -1,13 +1,16 @@
 package saim.hassan.walkthroughscreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
@@ -48,6 +51,24 @@ class MainActivity : AppCompatActivity() {
         })
         (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
                 RecyclerView.OVER_SCROLL_NEVER
+        findViewById<ImageView>(R.id.imageNext).setOnClickListener {
+            if (onboardingViewPager.currentItem+1 < onboardingItemsAdapter.itemCount){
+                onboardingViewPager.currentItem += 1
+            }else{
+                navigateToHomeActivity()
+            }
+        }
+        findViewById<TextView>(R.id.textSkip).setOnClickListener {
+            navigateToHomeActivity()
+        }
+        findViewById<MaterialButton>(R.id.buttonGetStarted).setOnClickListener {
+            navigateToHomeActivity()
+        }
+    }
+
+    private fun navigateToHomeActivity(){
+        startActivity(Intent(applicationContext,HomeActivity::class.java))
+        finish()
     }
 
     private fun setupIndicators(){
