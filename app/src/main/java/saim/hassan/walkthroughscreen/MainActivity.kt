d677eb06913a8ec4b5bc6d4e30c3c82934dafc2b
SAIM <saimh355@gmail.com>
2021-01-17 14:20:12 +0500

    Walkthrough Screen using ViewPager 2

---
+++ app/src/main/java/saim/hassan/walkthroughscreen/MainActivity.kt
@@ -0,0 +1,11 @@
+package saim.hassan.walkthroughscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
